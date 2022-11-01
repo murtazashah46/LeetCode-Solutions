562e2be50ec002b62d576785a055642276680665
murtazashah46 <86533000+murtazashah46@users.noreply.github.com>
2022-11-01 10:38:09 +0500

    Time: 221 ms (55.61%), Space: 18.7 MB (71.51%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,25 +1,4 @@
 import heapq
-class MedianFinder:
-    def __init__(self):
-        self.minHeap = []
-        self.maxHeap = []        
-    def addNum(self, num: int) -> None:
-        if(not self.maxHeap or -self.maxHeap[0] >= num):
-            heappush(self.maxHeap, -num)
-        else:
-            heappush(self.minHeap, num)
-        
-        if(len(self.maxHeap) > len(self.minHeap) + 1):
-            heappush(self.minHeap, -heappop(self.maxHeap))
-        elif(len(self.maxHeap) < len(self.minHeap)):
-            heappush(self.maxHeap, -heappop(self.minHeap))
-            
-    def findMedian(self) -> float:
-        if(len(self.minHeap) == len(self.maxHeap)):
-            return -self.maxHeap[0]/2 + self.minHeap[0]/2
-        else:
-            return -self.maxHeap[0] / 1
-
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         
